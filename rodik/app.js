e124c92b834b52ae031285a116efbd65f773a478
Rodik Hanukaev <rodik@chegg.com>
2016-02-11 20:54:53 +0200

    added example input

--- rodik/app.js
+++ rodik/app.js
@@ -13,9 +13,64 @@ function readFile(inputFile) {
     });
 }
 
+function linesToInt (lines) {
+    return lines.map(function (line) {
+       return splitLine(line);
+    });
+}
+function splitLine (str) {
+    return str.split(' ').map(function (x) {return parseInt(x);});
+}
+
+function getConfigs(lines) {
+    var rt = {
+        rows: lines[0][0],
+        cols: lines[0][1],
+        drones: lines[0][2],
+        deadline: lines[0][3],
+        maxPayload: lines[0][4],
+        products: lines[2]
+    };
+    return rt;
+}
+function getWarehouses(lines) {
+    var warehouses = [];
+    var numOfWarehouses = lines[3][0];
+    for (var i = 0; i<numOfWarehouses; i++) {
+        var loc = lines[4+(i*2)];
+        var inventory = lines[5+(i*2)];
+        warehouses[i] =  {
+            r: loc[0],
+            c: loc[1],
+            inventory: inventory
+        };
+    }
+    return warehouses;
+}
+function getOrders(lines, start, productTypesLength) {
+    var numOfOrders = lines[start];
+    var orders = [];
+    for (var i = 0; i<numOfOrders; i++) {
+        var loc = lines[(start+1)+(i*2)];
+        var itemsArr = [];
+        var items = lines[5+(i*2)];
+
+    }
+
+    return orders;
+}
 function run(inputFile) {
     readFile(inputFile).then(function(data) {
-        var lengths = data.slice(0,data.indexOf('\n')).split(' ');
+        var lines = linesToInt(data.split('\n'));
+        var configs = getConfigs(lines);
+        console.log('lines', lines);
+        //console.log('configs', configs);
+        var warehouses = getWarehouses(lines);
+        var orderStartLine = 4+(warehouses.length*2);
+        var orders = getOrders(lines, orderStartLine, configs.products.length);
+
         var input = data.slice(data.indexOf('\n')+1).split("\n").map(function(x){return x.split("");});
     });
 }
+
+run('./data/example.in');
