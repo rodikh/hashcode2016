be80e99c513a6eac1442acb3148594effbea6688
Rodik Hanukaev <rodik@chegg.com>
2016-02-11 20:10:10 +0200

    added input data

--- rodik/app.js
+++ rodik/app.js
@@ -1,3 +1,21 @@
 /**
  * Created by rodik on 2/11/16.
  */
+"use strict";
+var fs = require('fs');
+
+
+function readFile(inputFile) {
+    return new Promise(function(resolve, reject) {
+        fs.readFile(inputFile, 'utf8', function (err, data) {
+            resolve(data);
+        });
+    });
+}
+
+function run(inputFile) {
+    readFile(inputFile).then(function(data) {
+        var lengths = data.slice(0,data.indexOf('\n')).split(' ');
+        var input = data.slice(data.indexOf('\n')+1).split("\n").map(function(x){return x.split("");});
+    });
+}
